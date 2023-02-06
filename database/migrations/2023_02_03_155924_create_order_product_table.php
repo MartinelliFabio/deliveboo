bb5a2b5e8dcece04c68793c8b45c46b2004fcafa
MartinelliFabio <fabio.martinelli996@gmail.com>
2023-02-06 19:23:24 +0100

    fixed pivot order-product table

--- database/migrations/2023_02_03_155924_create_order_product_table.php
+++ database/migrations/2023_02_03_155924_create_order_product_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('order_product', function (Blueprint $table) {
             $table->id();
+            $table->float('price', 6, 2);
+            $table->tinyInteger('quantity');
             $table->foreignId('product_id')->onUpdate('cascade')->onDelete('cascade')->constrained();
             $table->foreignId('order_id')->onUpdate('cascade')->onDelete('cascade')->constrained();
         });
